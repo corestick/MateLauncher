02982eaf82fd874b1af9ff883b571f3291d49dbc
Ryu Jongwon <corestick@gmail.com>
2013-03-28 20:24:16 +0900

    패키지명 변경

---
+++ MateLauncher/gen/mobi/intuitit/android/mate/launcher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package mobi.intuitit.android.mate.launcher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
