2375b3426274163855e5472a000032cf90c531ad
Ryu Jongwon <corestick@gmail.com>
2013-04-01 21:45:20 +0900

    Layout, SurfaceView 생성

---
+++ MateLauncher/src/mobi/intuitit/android/mate/launcher/MSFView.java
@@ -0,0 +1,13 @@
+package mobi.intuitit.android.mate.launcher;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+
+public class MSFView extends GLSurfaceView {
+
+	public MSFView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+}
