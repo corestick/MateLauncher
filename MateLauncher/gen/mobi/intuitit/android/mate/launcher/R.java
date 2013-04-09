1cf73dd575511f8d17938937bf196541d3870e9b
Ryu Jongwon <corestick@gmail.com>
2013-04-09 22:32:03 +0900

    수정

--- MateLauncher/gen/mobi/intuitit/android/mate/launcher/R.java
+++ MateLauncher/gen/mobi/intuitit/android/mate/launcher/R.java
@@ -308,15 +308,13 @@ containing a value of this type.
         public static final int live_folder_grid=0x7f030009;
         public static final int live_folder_icon=0x7f03000a;
         public static final int live_folder_list=0x7f03000b;
-        public static final int msfview=0x7f03000c;
-        public static final int rename_folder=0x7f03000d;
-        public static final int screen_switcher=0x7f03000e;
-        public static final int user_folder=0x7f03000f;
-        public static final int wallpaper_chooser=0x7f030010;
-        public static final int wallpaper_item=0x7f030011;
-        public static final int widget_search=0x7f030012;
-        public static final int workspace_mscreen=0x7f030013;
-        public static final int workspace_screen=0x7f030014;
+        public static final int rename_folder=0x7f03000c;
+        public static final int screen_switcher=0x7f03000d;
+        public static final int user_folder=0x7f03000e;
+        public static final int wallpaper_chooser=0x7f03000f;
+        public static final int wallpaper_item=0x7f030010;
+        public static final int widget_search=0x7f030011;
+        public static final int workspace_screen=0x7f030012;
     }
     public static final class string {
         /**  Displayed when user selects a shortcut for an app that was uninstalled 
