a4f27b01c9dd4b98310c99a14c82dc9ac89865b6
nadongkyu <kaineus86@gmail.com>
2013-05-28 21:30:16 +0900

    sdaf

--- MateLauncher/src/mobi/intuitit/android/mate/launcher/MDockbar.java
+++ MateLauncher/src/mobi/intuitit/android/mate/launcher/MDockbar.java
@@ -2,12 +2,10 @@ package mobi.intuitit.android.mate.launcher;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Environment;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,15 +13,12 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MDockbar extends LinearLayout implements View.OnClickListener {
-
-	public final int HIDE_MODE = 0;
-	public final int FURNITURE_MODE = 1;
-	public final int WALLPAPER_MODE = 2;
-	public final int FLOORING_MODE = 3;
-	public final int AVATAR_MODE = 4;
-
-	public int mMDockbarMode = HIDE_MODE;
-
+	
+	private final int FURNITURE = 1;
+	private final int WALLPAPER = 2;
+	private final int FLOORING = 3;
+	private final int AVATAR = 4;
+	
 	private ImageButton mFurniture;
 	private ImageButton mWallpaper;
 	private ImageButton mFlooring;
@@ -31,21 +26,11 @@ public class MDockbar extends LinearLayout implements View.OnClickListener {
 	private ImageButton mHomepage;
 	private ImageButton mRight;
 
-	Launcher launcher;
-	
-
+	Launcher mLauncher;
 	Context context;
-
-	MobjectAdapter mFurnitureAdapter;
-	MobjectAdapter mWallpaperAdapter;
-	MobjectAdapter mFlooringAdapter;
-	MobjectAdapter mAvatarAdapter;
-	ArrayList<Mobject> mFurnitureList;
-	ArrayList<Mobject> mWallpaperList;
-	ArrayList<Mobject> mFlooringList;
-	ArrayList<Mobject> mAvatarList;
-
-	Bitmap bm[] = new Bitmap[3]; // 스크린샷용 비트맵
+	
+	Bitmap captureView[]; 
+	
 
 	public MDockbar(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -56,7 +41,7 @@ public class MDockbar extends LinearLayout implements View.OnClickListener {
 	}
 
 	public void initMDockbar(Launcher launcher) {
-		this.launcher = launcher;
+		this.mLauncher = launcher;
 
 		mFurniture = new ImageButton(context);
 		mFlooring = new ImageButton(context);
@@ -108,52 +93,7 @@ public class MDockbar extends LinearLayout implements View.OnClickListener {
 		mAvatar.setOnClickListener(this);
 		mHomepage.setOnClickListener(this);
 		mRight.setOnClickListener(this);
-
-		initAdapter();
-	}
-
-	public void initAdapter() {
-		mFurnitureList = new ArrayList<Mobject>();
-		mWallpaperList = new ArrayList<Mobject>();
-		mFlooringList = new ArrayList<Mobject>();
-		mAvatarList = new ArrayList<Mobject>();
-
-		for (int i = 0; i < MImageList.getInstance().furnitureList.size(); i++) {
-			Mobject mObject = new Mobject();
-			mObject.icon = getResources().getDrawable(
-					MImageList.getInstance().furnitureList.get(i));
-
-			mFurnitureList.add(mObject);
-		}
-		mFurnitureAdapter = new MobjectAdapter(launcher, mFurnitureList);
-
-		for (int i = 0; i < MImageList.getInstance().wallpaperList.size(); i++) {
-			Mobject mObject = new Mobject();
-			mObject.icon = getResources().getDrawable(
-					MImageList.getInstance().wallpaperList.get(i));
-
-			mWallpaperList.add(mObject);
-		}
-		mWallpaperAdapter = new MobjectAdapter(launcher, mWallpaperList);
-
-		for (int i = 0; i < MImageList.getInstance().flooringList.size(); i++) {
-			Mobject mObject = new Mobject();
-			mObject.icon = getResources().getDrawable(
-					MImageList.getInstance().flooringList.get(i));
-
-			mFlooringList.add(mObject);
-		}
-		mFlooringAdapter = new MobjectAdapter(launcher, mFlooringList);
-
-		for (int i = 0; i < MImageList.getInstance().avatarList.size(); i++) {
-			Mobject mObject = new Mobject();
-			mObject.icon = getResources().getDrawable(
-					MImageList.getInstance().avatarList.get(i));
-
-			mAvatarList.add(mObject);
-		}
-		mAvatarAdapter = new MobjectAdapter(launcher, mAvatarList);
-	}
+	}	
 
 	public void showMDockbar() {
 		this.setVisibility(VISIBLE);
@@ -162,125 +102,89 @@ public class MDockbar extends LinearLayout implements View.OnClickListener {
 	public void hideMDockbar() {
 		this.setVisibility(INVISIBLE);
 	}
-
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stubs
 
 		if (v.equals(mFurniture)) {
-			if (mMDockbarMode == FURNITURE_MODE) {
-				launcher.mObjectView.setVisibility(View.GONE);
-				mMDockbarMode = HIDE_MODE;
+			if (mLauncher.mObjectView.mObjectViewType == FURNITURE) {
+				mLauncher.mObjectView.hideMobjectView();
 			} else {
-				launcher.mObjectView.setAdapter(mFurnitureAdapter);
-				launcher.mObjectView.setVisibility(View.VISIBLE);
-				mMDockbarMode = FURNITURE_MODE;
+				mLauncher.mObjectView.showMojbectView(FURNITURE);
 			}
 			return;
 		}
 
 		if (v.equals(mWallpaper)) {
 
-			if (mMDockbarMode == WALLPAPER_MODE) {
-				launcher.mObjectView.setVisibility(View.GONE);
-				mMDockbarMode = HIDE_MODE;
+			if (mLauncher.mObjectView.mObjectViewType == WALLPAPER) {
+				mLauncher.mObjectView.hideMobjectView();
 			} else {
-				launcher.mObjectView.setAdapter(mWallpaperAdapter);
-				launcher.mObjectView.setVisibility(View.VISIBLE);
-				mMDockbarMode = WALLPAPER_MODE;
+				mLauncher.mObjectView.showMojbectView(WALLPAPER);
 			}
 			return;
 		}
 
 		if (v.equals(mFlooring)) {
-			if (mMDockbarMode == FLOORING_MODE) {
-				launcher.mObjectView.setVisibility(View.GONE);
-				mMDockbarMode = HIDE_MODE;
+			if (mLauncher.mObjectView.mObjectViewType == FLOORING) {
+				mLauncher.mObjectView.hideMobjectView();
 			} else {
-				launcher.mObjectView.setAdapter(mFlooringAdapter);
-				launcher.mObjectView.setVisibility(View.VISIBLE);
-				mMDockbarMode = FLOORING_MODE;
+				mLauncher.mObjectView.showMojbectView(FLOORING);
 			}
 			return;
 		}
 
 		if (v.equals(mAvatar)) {
-			if (mMDockbarMode == AVATAR_MODE) {
-				launcher.mObjectView.setVisibility(View.GONE);
-				mMDockbarMode = HIDE_MODE;
+			if (mLauncher.mObjectView.mObjectViewType == AVATAR) {
+				mLauncher.mObjectView.hideMobjectView();
 			} else {
-				launcher.mObjectView.setAdapter(mAvatarAdapter);
-				launcher.mObjectView.setVisibility(View.VISIBLE);
-				mMDockbarMode = AVATAR_MODE;
+				mLauncher.mObjectView.showMojbectView(AVATAR);
 			}
 			return;
 		}
-		if (v.equals(mHomepage)) {
-			// 캡쳐
-			final int count = launcher.getWorkspace().getChildCount();
-			LayoutType screenView;
+		if (v.equals(mHomepage)) {		
+			//captureView 생성
+			int count = mLauncher.getWorkspace().getChildCount();
+			if (captureView == null || captureView.length != count)
+				captureView = new Bitmap[count];
+			
 			String sdcard = Environment.getExternalStorageDirectory()
 					.getAbsolutePath();
 			File cfile = new File(sdcard+ "/Test");
 			cfile.mkdirs();
-//			View captureView = launcher.getWorkspace();
-//			captureView.getRootView().buildDrawingCache();
-//			View captureView = (LayoutType) launcher.getWorkspace().getChildAt(i);
-//			captureView.buildDrawingCache();
-//			bm = captureView.getDrawingCache();		
+			
+			LayoutType screenView;					
 			
 			for (int i = 0; i < count; i++) {
-				// Prepare Cache
-				screenView = (LayoutType) launcher.getWorkspace().getChildAt(i);
+				screenView = (LayoutType) mLauncher.getWorkspace().getChildAt(i);
 				screenView.saveThumb();
-				bm[i] = screenView.getThumb();
+				captureView[i] = screenView.getThumb();
 				
-				String path = sdcard + "/Test/"
-						+ "screen"+i+".jpg";		
+				String path = sdcard + "/Test/"+"screen"+i+".jpg";		
 				try {
 					FileOutputStream fos = new FileOutputStream(path);
-					bm[i].compress(Bitmap.CompressFormat.JPEG, 100, fos);
+					captureView[i].compress(Bitmap.CompressFormat.JPEG, 100, fos);
 					fos.flush();
 					fos.close();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-//				Intent intentScreen = new Intent(
-//						Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-//				Uri uri = Uri.parse("file://" + path);
-//				intentScreen.setData(uri);
-//				launcher.sendBroadcast(intentScreen);
 			}
-
-			// 런처 홈으로 이동
 			Intent intent = new Intent(Intent.ACTION_MAIN);
 			intent.setClassName("com.LBL.launcherhome",
 					"com.LBL.launcherhome.Main");
 			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			intent.addCategory(Intent.CATEGORY_LAUNCHER);
-			launcher.startActivity(intent);
+			mLauncher.startActivity(intent);
 			return;
 		}
 
 		if (v.equals(mRight)) {
-			mMDockbarMode = HIDE_MODE;
-			launcher.mObjectView.setVisibility(View.GONE);
+			mLauncher.mObjectView.hideMobjectView();
 			hideMDockbar();
 			Launcher.modifyMode = false;
-			launcher.mDockbar.showDockbar();
+			mLauncher.mDockbar.showDockbar();
 			return;
 		}
-	}
-
-	public boolean isDraggable() {
-		switch (mMDockbarMode) {
-		case FURNITURE_MODE:
-		case AVATAR_MODE:
-			return true;
-		case WALLPAPER_MODE:
-		case FLOORING_MODE:
-			return false;
-		}
-		return false;
 	}
 }
